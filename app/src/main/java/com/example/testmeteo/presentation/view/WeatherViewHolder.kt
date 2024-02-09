674dd797c6b7e94178309287b53e4ebf3e6006af
Augustin Devaux <74774090+AugustinDevaux@users.noreply.github.com>
2024-02-09 10:28:38 +0100

    WeatherViewHolder

--- app/src/main/java/com/example/testmeteo/presentation/view/WeatherViewHolder.kt
+++ app/src/main/java/com/example/testmeteo/presentation/view/WeatherViewHolder.kt
@@ -1,2 +1,28 @@
 package com.example.testmeteo.presentation.view
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.testmeteo.R
+import com.example.testmeteo.data.remote.Weather
+
+
+class WeatherViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    fun bind(weather: Weather) {
+// Bind data to views
+        val textViewTemperature = itemView.findViewById<TextView>(R.id.textViewTemperature)
+        val textViewHumidity = itemView.findViewById<TextView>(R.id.textViewHumidity)
+        val textViewDescription = itemView.findViewById<TextView>(R.id.textViewDescription)
+        val imageViewIcon = itemView.findViewById<ImageView>(R.id.imageViewIcon)
+        textViewTemperature.text = "Temperature: ${weather.main.temp}°C"
+        textViewHumidity.text = "Humidity: ${weather.main.humidity}%"
+        textViewDescription.text = "Description: ${weather.weather[0].description}"
+// Load weather icon using Glide
+        val iconUrl = "https://openweathermap.org/img/wn/${weather.weather.first().icon}.png"
+        Glide.with(itemView.context)
+            .load(iconUrl)
+            .into(imageViewIcon)
+    }
+}
